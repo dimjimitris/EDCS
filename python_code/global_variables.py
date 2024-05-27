9ef3ab7785e203cd83d368b9bdb53be1b22567ce
ntua-el19005 <el19005@mail.ntua.gr>
2024-05-27 10:52:35 +0200

    environment (global variables) set by a .env file now

--- python_code/global_variables.py
+++ python_code/global_variables.py
@@ -1,22 +1,20 @@
 # global_variables.py
 # Description: This file contains the global variables used in the project.
+from dotenv import load_dotenv
+import os
 
-HEADER_LENGTH = 64
-FORMAT = "utf-8"
+# Load variables from .env file
+load_dotenv()
 
-CONNECTION_TIMEOUT = 5
-LEASE_TIMEOUT = 5
-
-SERVERS = [
-    ("localhost", 5000),
-    ("localhost", 5001),
-    ("localhost", 5002),
-]
-
-MEMORY_SIZE = 300
-
-# communication message status
-SUCCESS = 0
-ERROR = 1
-INVALID_ADDRESS = 2
-INVALID_OPERATION = 3
+# Define global variables
+HEADER_LENGTH = int(os.getenv("HEADER_LENGTH"))                                     # 64
+FORMAT = os.getenv("FORMAT")                                                        # 'utf-8'               
+CONNECTION_TIMEOUT = int(os.getenv("CONNECTION_TIMEOUT"))                           # 5
+LEASE_TIMEOUT = int(os.getenv("LEASE_TIMEOUT"))                                     # 5    
+SERVERS = [tuple(server.split(":")) for server in os.getenv("SERVERS").split(",")]
+SERVERS = [(server[0], int(server[1])) for server in SERVERS] 
+MEMORY_SIZE = int(os.getenv("MEMORY_SIZE"))                                         # 300
+SUCCESS = int(os.getenv("SUCCESS"))                                                 # 0      
+ERROR = int(os.getenv("ERROR"))                                                     # 1
+INVALID_ADDRESS = int(os.getenv("INVALID_ADDRESS"))                                 # 2
+INVALID_OPERATION = int(os.getenv("INVALID_OPERATION"))                             # 3
